26b7bf3706a1f1c4a13a09eab3b46c2091de345c
Antoine Neveux a183276 <antoine.neveux@worldline.com>
2016-04-22 13:32:00 +0200

    Added Java optimizations for all extensions -> now available under ResultToolbox. Minor cleanup in pom.xml

--- src/main/kotlin/com/github/ktoolz/rezult/VoidToUnit.kt
+++
@@ -1,9 +0,0 @@
-@file:JvmName("ResultToolbox")
-package com.github.ktoolz.rezult
-
-import java.util.function.Consumer
-
-
-fun <T> toUnit(execute: Consumer<T>): Function1<T, Unit> {
-    return { t -> execute.accept(t) }
-}
